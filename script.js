fb0e8bae697ce08e40133e28f5cfab80703b4c1e
Ricardo Hernandez <richhquim@gmail.com>
2025-05-05 15:16:39 -0600

    dos

--- script.js
+++ script.js
@@ -1,52 +1,137 @@
 const cursos = [
-    {
-      nombre: "Programación Web",
-      profesor: "Kevin",
-      estudiantes: [
-        { nombre: "Ana", nota: 8 },
-        { nombre: "Luis", nota: 6 },
-        { nombre: "Sofía", nota: 9 },
-        { nombre: "Carlos", nota: 7 }
-      ],
-    },
-    {
-      nombre: "Bases de Datos",
-      profesor: "Diego",
-      estudiantes: [
-        { nombre: "Pedro", nota: 5 },
-        { nombre: "Sofía", nota: 7 },
-        { nombre: "Luis", nota: 4 },
-        { nombre: "Ana", nota: 6 }
-      ],
-    },
-    {
-      nombre: "Diseño UX/UI",
-      profesor: "Lucía",
-      estudiantes: [
-        { nombre: "Ana", nota: 10 },
-        { nombre: "Pedro", nota: 6 },
-        { nombre: "Valentina", nota: 9 },
-        { nombre: "Carlos", nota: 10 }
-      ],
-    },
-    {
-      nombre: "Lógica de Programación",
-      profesor: "Martín",
-      estudiantes: [
-        { nombre: "Luis", nota: 5 },
-        { nombre: "Cristina", nota: 8 },
-        { nombre: "Ricardo", nota: 9 },
-        { nombre: "Sofía", nota: 6 }
-      ],
-    },
-    {
-      nombre: "JavaScript Intermedio",
-      profesor: "Mariana",
-      estudiantes: [
-        { nombre: "Kevin", nota: 7 },
-        { nombre: "Valentina", nota: 8 },
-        { nombre: "Pedro", nota: 9 },
-        { nombre: "Ana", nota: 10 }
-      ],
-    }
-  ];
+  {
+    nombre: "Programación Web",
+    profesor: "Kevin",
+    estudiantes: [
+      { nombre: "Ana", nota: 8 },
+      { nombre: "Luis", nota: 6 },
+      { nombre: "Sofía", nota: 9 },
+      { nombre: "Carlos", nota: 7 }
+    ],
+  },
+  {
+    nombre: "Bases de Datos",
+    profesor: "Diego",
+    estudiantes: [
+      { nombre: "Pedro", nota: 5 },
+      { nombre: "Sofía", nota: 7 },
+      { nombre: "Luis", nota: 4 },
+      { nombre: "Ana", nota: 6 }
+    ],
+  },
+  {
+    nombre: "Diseño UX/UI",
+    profesor: "Lucía",
+    estudiantes: [
+      { nombre: "Ana", nota: 10 },
+      { nombre: "Pedro", nota: 6 },
+      { nombre: "Valentina", nota: 9 },
+      { nombre: "Carlos", nota: 10 }
+    ],
+  },
+  {
+    nombre: "Lógica de Programación",
+    profesor: "Martín",
+    estudiantes: [
+      { nombre: "Luis", nota: 5 },
+      { nombre: "Cristina", nota: 8 },
+      { nombre: "Ricardo", nota: 9 },
+      { nombre: "Sofía", nota: 6 }
+    ],
+  },
+  {
+    nombre: "JavaScript Intermedio",
+    profesor: "Mariana",
+    estudiantes: [
+      { nombre: "Kevin", nota: 7 },
+      { nombre: "Valentina", nota: 8 },
+      { nombre: "Pedro", nota: 9 },
+      { nombre: "Ana", nota: 10 }
+    ],
+  }
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//ubicacion de Luis
+console.log(cursos[3].estudiantes[0].nombre)
+
+//Ubicacion de la nota de Luis
+console.log(cursos[3].estudiantes[0].nota)
+console.log(cursos[0].estudiantes.length)
+
+
+for (let j = 0; j < cursos[0].estudiantes.length; j++) {
+  let Luis = cursos.find((curso) => curso.estudiantes[j].name === "Luis")
+
+  console.log(Luis)
+}
+
+
+
+//ubicacion de la nota de Ana (el problema es que hay tres estudiantes Ana)
+
+
+
+
+
+
+
+
+
+
+
